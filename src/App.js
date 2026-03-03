d145a641b889b4a491aece9cb198f5277c3211b2
Dharunpandi <dharunpandi754@gmail.com>
2026-03-03 21:44:32 +0530

    aklsfj

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
 import "./App.css";
 import Home from "./pages/Home";
+import * as Sentry from "@sentry/react";
 
 function App() {
-  // Test error
-  throw new Error("Sentry test error from App.js!");
+  // Function to safely send a test error to Sentry
+  const sendTestError = () => {
+    Sentry.captureException(new Error("Test error from App.js"));
+    alert("Sentry test error sent!");
+  };
 
-  return <Home />;
+  return (
+    <div>
+      {/* Your Home component */}
+      <Home />
+
+      {/* Safe button to test Sentry */}
+      <button 
+        onClick={sendTestError} 
+        style={{
+          marginTop: "20px",
+          padding: "10px 15px",
+          backgroundColor: "#f44336",
+          color: "white",
+          border: "none",
+          borderRadius: "5px",
+          cursor: "pointer"
+        }}
+      >
+        Send Test Error to Sentry
+      </button>
+    </div>
+  );
 }
 
 export default App;
