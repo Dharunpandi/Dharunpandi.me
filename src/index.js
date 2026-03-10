9130e30ad84d5b8e64d98fd15f999fbf5367fba6
Dharunpandi <dharunpandi754@gmail.com>
2026-03-10 11:56:04 +0530

    final commit

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import * as Sentry from "@sentry/react";
 import "./index.css";
 import App from "./App";
 
-// Initialize Sentry
 Sentry.init({
   dsn: "https://674578bb53f63cc263e1b420aa5efaaf@o4510979721396224.ingest.us.sentry.io/4510980882563072",
   sendDefaultPii: true
@@ -14,5 +13,5 @@ ReactDOM.render(
   <Sentry.ErrorBoundary fallback={<div>An error has occurred</div>}>
     <App />
   </Sentry.ErrorBoundary>,
-  document.getElementById("app")
+  document.getElementById("root")   // ✅ must match index.html
 );
